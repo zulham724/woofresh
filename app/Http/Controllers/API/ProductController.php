f8b623fd1ba53e5ec54b03bfc2b0b7958562ce8a
mahluz <zulham724@gmail.com>
2018-09-14 15:08:16 +0700

    small update

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -23,6 +23,7 @@ class ProductController extends Controller
     {
         $products = Product::
         with('supplier')
+        ->with('product_sales')
         ->with('product_translations.language')
         ->get();
         return response()->json($products);
