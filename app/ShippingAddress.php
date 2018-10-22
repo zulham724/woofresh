28462247d26d6097a1e38a8c29f26d26207d7263
ZULHAM AZWAR ACHMAD <zulham724@students.unnes.ac.id>
2018-10-22 16:28:37 +0700

    update product images

--- app/ShippingAddress.php
+++ app/ShippingAddress.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShippingAddress extends Model
 {
+	protected $guarded = ["id"];
     public function transaction(){
     	return $this->belongsTo('App\Transaction');
     }
